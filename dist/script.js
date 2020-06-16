73d9153babfccfc67330aabca201115b749d865c
Sielik <kr.sielicki@gmail.com>
2020-06-16 11:26:55 +0200

    dist

---
+++ dist/script.js
@@ -0,0 +1,23 @@
+const screen = document.querySelector(".screen");
+const btns = document.querySelectorAll(".btn");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let number = btns[i].getAttribute("data-num");
+    screen.value += number;
+  });
+}
+
+equalBtn.addEventListener("click", function () {
+  if (screen.value === "") {
+    screen.value = value;
+  }
+  let value = eval(screen.value);
+  screen.value = value;
+});
+
+clearBtn.addEventListener("click", function () {
+  screen.value = "";
+});
